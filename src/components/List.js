5729589c7dba2fdf7411e43037d1284c4da9d4d2
Kendall Arneaud <kendall.arneaud@gmail.com>
2021-11-07 16:35:57 +0000

    add props. refactor component markup.

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 class List extends React.Component {
+	
+  constructor(props) {
+  	super(props);
+  }
+
   render() { 
   	return (
    	 	<div>
-    	List
-    </div>
+    		List
+    		<ol>{ this.props.items.length > 0 ? this.props.items.map( (item, k) => (<li key={ `product-item-${k}` }><h1>{ item.name }</h1></li>) ) : <li>No Data</li> }</ol>
+    	</div>
   	  ); 
 	}
 }
