c01a0b592e169f6ffbb9a4d92361fcd11e79a4c9
Kendall Arneaud <kendall.arneaud@gmail.com>
2021-11-07 16:36:45 +0000

    add render with router function

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,11 +2,11 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import BrowserRouter from 'react-router-dom';
+import '@testing-library/jest-dom';
 
-function renderWithRouter (ui, {route = '/'} = {}) {
+const renderWithRouter = (ui, {route = '/'} = {})  => {
   window.history.pushState({}, 'Test page', route)
 
   return render(ui, {wrapper: BrowserRouter})
