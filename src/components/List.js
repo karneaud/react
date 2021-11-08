e5b6afe50d656b37f0be8c7fb91c6518d36b4ba1
Kendall Arneaud <kendall.arneaud@gmail.com>
2021-11-08 00:32:55 +0000

    modified markup

--- src/components/List.js
+++ src/components/List.js
@@ -2,16 +2,18 @@ import React from 'react';
 
 class List extends React.Component {
 	
-  constructor(props) {
-  	super(props);
-  }
-
   render() { 
   	return (
-   	 	<div>
-    		List
-    		<ol>{ this.props.items.length > 0 ? this.props.items.map( (item, k) => (<li key={ `product-item-${k}` }><h1>{ item.name }</h1></li>) ) : <li>No Data</li> }</ol>
-    	</div>
+   	 	<section>
+    		<article>
+    		<ol>{ 
+            	this.props.items && this.props.items.length > 0? 
+            		this.props.items.map( (item, k) => 
+    						(<li key={ `product-item-${k}` }>
+								<h1>{ item.name }</h1>
+							</li>) ) : <li>No Data</li> }</ol>
+    		</article>
+		</section>
   	  ); 
 	}
 }
