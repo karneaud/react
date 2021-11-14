81be5b22c89fb80b36b4902a5579a6cb985c14c5
Kendall Arneaud <kendall.arneaud@gmail.com>
2021-11-14 01:41:59 +0000

    test trigger select category handlers

--- src/components/Category.test.js
+++ src/components/Category.test.js
@@ -1,18 +1,25 @@
-import { render, screen, container } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import Category from './Category';
 
-const data = [ "tech" , "clothes"];
+const items = [ { name: "tech" }, { name: "clothes" }];
 
+Category.prototype.componentDidMount = function() { this.setState({ items, selected: "clothes" }) };
 
 describe('test category component', () => {
-	test('should be a category component with no menu items', () => {
- 		render(<Category items={ [] } />);
- 		expect(screen.getByText('No Data')).toBeInTheDocument();
-	});
-
 	test('should have menu item elements', () => {
-    	  render(<Category items={ data } />);
+    		render(<Category />, { wrapper: Category });
  		 expect(screen.getByText('clothes')).toBeTruthy();
+    });
+
+	test('should return category name onCategorySelected', () => {
+    	const testCategorySelected = jest.fn(),
+              { container } = render(<Category onCategorySelected={ testCategorySelected } />, { wrapper: Category } );
+        	waitFor(async () => {
+    			await fireEvent.click(screen.getByText('tech'));
+    			expect(testCategorySelected).toHaveBeenCalledTimes(1);
+				expect(testCategorySelected).toHaveBeenCalledWith('tech');
+				expect(container.querySelector('.selected')).toBeTruthy();
+    		});
     });
 })
 
