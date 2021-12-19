82ef59b501145fdf649e6290fccb788757388cdd
karneaud <kendall.arneaud@gmail.com>
2021-12-19 14:03:17 -0400

    add global state. update global state

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { client, Query, Field } from '@tilework/opus';
+import { client, Query } from '@tilework/opus';
+import { set } from '../state';
 
 const query = new Query('categories', true) 
 		.addFieldList( ['name'] );
@@ -8,25 +9,23 @@ class Category extends React.PureComponent {
   
 	constructor(props) {
     	super(props);
-    	this.state = { items: [], selected: null };
+    	this.state = { items: [], category: null };
    }
 
-	selectCategory(selected) {
-    	this.setState({ selected });
+	selectCategory(category) {
+    	this.setState({ category });
     }
+
+	componentDidUpdate(p, s) {
+		if(s.category !== this.state.category) set({  category: this.state.category })
+	}
 
 	componentDidMount() {
   		client.post(query).then((data) => {
     		let items = data.categories, category = items[0].name;
-  			this.setState({ items, selected: category });
+  			this.setState({ items, category });
     	}).catch(e => console.log(e))
   	}
-	
-	componentDidUpdate(p, s) {
-    	if((s.selected != this.state.selected) && 
-           this.props.onCategorySelected) this.props.onCategorySelected(this.state.selected);
-    }
-	
 	
 
    render() { 
@@ -35,8 +34,8 @@ class Category extends React.PureComponent {
    	 	{ this.state.items && this.state.items.length > 0 ? 
                  	this.state.items.map( 
                  			(item, k) => 
-    							(<li className={ `category-menu-item ${ this.state.selected == item? 'selected' : ''  }`  }  key={ `category-menu-item-${k}` }>
-									<a href="#" className="btn btn-link" onClick={ this.selectCategory.bind(this, item.name) }><span>{ item.name }</span></a>
+    							(<li className={ `category-menu-item ${ this.state.category === item? 'selected' : ''  }`  }  key={ `category-menu-item-${k}` }>
+									<a href={`#${ item.name }`} className="btn btn-link" onClick={ this.selectCategory.bind(this, item.name) }><span>{ item.name }</span></a>
 								</li>) 
 							) : <li><h2>No Categories</h2></li> }
     	</React.Fragment>
